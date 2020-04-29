5a8ca94ad531748b836ac03bf29f3d21ec7d93b7
Dan Stutzman <dan@danstutzman.com>
2020-04-29 13:35:20 -0600

    Update rollout script

--- rollout.sh
+++ rollout.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -ex
-BUCKET_NAME=danstutzman.com
+BUCKET_NAME=danstutzman-www-danstutzman-com
 
 bundle exec serve export . output
 
-aws s3 mb s3://$BUCKET_NAME
-aws s3 website s3://$BUCKET_NAME --index-document index.html
+AWS_PROFILE=personal aws s3 mb s3://$BUCKET_NAME
+AWS_PROFILE=personal aws s3 website s3://$BUCKET_NAME --index-document index.html
 
 cat >policy.json <<EOF
 {
@@ -14,13 +14,13 @@ cat >policy.json <<EOF
     "Effect":"Allow",
     "Principal": "*",
     "Action":["s3:GetObject"],
-    "Resource":["arn:aws:s3:::danstutzman.com/*"]
+    "Resource":["arn:aws:s3:::$BUCKET_NAME/*"]
   }]
 }
 EOF
-aws s3api put-bucket-policy --bucket danstutzman.com --policy file://policy.json
+AWS_PROFILE=personal aws s3api put-bucket-policy --bucket $BUCKET_NAME --policy file://policy.json
 rm policy.json
 
-aws s3 sync --exclude ".*" output s3://$BUCKET_NAME
+AWS_PROFILE=personal aws s3 sync --exclude ".*" output s3://$BUCKET_NAME
 
 open http://$BUCKET_NAME.s3-website-us-east-1.amazonaws.com
